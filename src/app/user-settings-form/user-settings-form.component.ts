58e80c18b83f9d18ca2ecd28a4702fafac5b9cc9
catgsmith <catgsmith@hotmail.com>
2021-03-27 15:51:20 +0000

    create data service

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -17,14 +18,18 @@ export class UserSettingsFormComponent implements OnInit {
   };
   userSettings: UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
   onSubmit(form: NgForm) {
-      console.log('in onSubmit', form.valid);      
+      console.log('in onSubmit', form.valid); 
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('success: ', result),
+        error => console.log('error: ', error)        
+      );     
   }
   onBlur(field: NgModel) {
-     console.log('in onBlur', field.valid);     
+     console.log('in onBlur', field.valid);
   }
 }
